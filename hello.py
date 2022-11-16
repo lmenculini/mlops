7bab109323eedb8f7009b87bc3152def3b39cf67
Lorenzo <lorenzo.menculini@gmail.com>
2022-11-16 18:47:09 +0100

    comment change

--- hello.py
+++ hello.py
@@ -2,4 +2,4 @@ print("Hello, I was modified in a new branch")
 
 print("Hey, I was added on Wednesday Nov 16th on a new branch!")
 
-# comment added on GitHub
+# comment added on GitHub, changed locally
