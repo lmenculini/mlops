f9aa3cd5fb3ba4d8de5b3574aa79c7478318187b
Lorenzo Menculini <62011313+lorenzo-menculini@users.noreply.github.com>
2022-11-16 18:48:03 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,3 +3,5 @@ print("Hello, I was modified in a new branch")
 print("Hey, I was added on Wednesday Nov 16th on a new branch!")
 
 # comment added on GitHub, changed locally
+
+print("Writing on GitHub")
